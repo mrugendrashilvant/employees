56a9fa74a21f07365dcd04f45eba11dfe9f0c271
Mrugendra Shilvant <sanketshilvant@gmail.com>
2021-11-10 17:36:07 +0530

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_061341) do
+ActiveRecord::Schema.define(version: 2021_11_10_101018) do
 
   create_table "employee_details", force: :cascade do |t|
     t.string "fName"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_11_10_061341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"

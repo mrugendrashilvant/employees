5d30ac665e340c140cc441e7d7482feee5f1bb81
lanchi <lanchiagrawal11@gmail.com>
2021-11-10 16:36:37 +0530

    Add validation

--- app/models/employee_detail.rb
+++ app/models/employee_detail.rb
@@ -1,2 +1,6 @@
 class EmployeeDetail < ApplicationRecord
+    validates :fName, :lName, :email, presence: true
+    validates :fName, length: { minimum: 3 }
+    validates :experience, numericality: true
+    validates :email, uniqueness: true
 end
